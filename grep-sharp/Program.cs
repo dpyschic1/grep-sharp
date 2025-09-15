1cbb244fbda3548afc9ed55bdd1f3cb803eb1d2c
Mubashir <mubashirsoomro60@gmail.com>
2025-09-15 16:40:13 -0400

    Commandline parsing

--- grep-sharp/Program.cs
+++ grep-sharp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var options = new Dictionary<string, string>();
+string input = string.Empty;
+for(int i = 0; i < args.Length; i++)
+{
+    if (args[i].StartsWith("-"))
+    {
+        var option = args[i].TrimStart('-');
+        var value = (i + 1 < args.Length && !args[i + 1].StartsWith("-")) ? args[++i] : "true";
+        options[option] = value.ToUpper();
+    }
+    else
+    {
+        input = args[i];
+    }
+}
+
+if (!options.TryGetValue("E", out var pattern))
+{
+    Console.WriteLine("Pattern string not provided");
+}
+
+
